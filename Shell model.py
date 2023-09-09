a8aee94338ce3589cc85fcf7eb82a95886197396
KrishnadevV015 <134763950+KrishnadevV015@users.noreply.github.com>
2023-09-09 21:31:09 +0530

    Update Shell model.py

--- Shell model.py
+++ Shell model.py
@@ -43,7 +43,10 @@ def E(N,l,s): #Defining total energy
 def n(N,l): # n to label
     return int((N-l+2)/2)
 
-SY=['s','p','d','f','g','h','i'] #list contatining letters corresponing to different l values
+SY=['s','p','d','f','g','h','i']#list contatining letters corresponing to different l values
+
+colors=['olive','green','orange','cyan','blue','purple','grey'] #list containing colors, each color is to be given to levels with same l value
+label_coordinates=[3.1,3.4,3.7,4,4.3,4.6,4.9,5.2] #list containing x coordinates where we have to add the label for each level, each coordinate is to lebel levels with same l value
 
 N_values=arange(0,6,1) #List of Harmonic quantum numbers
 
@@ -56,7 +59,7 @@ for N in N_values: #to genrate Harmonic energy term
     for l in l_valus:   #to genrate Harmonic energy term + centrifugal energy term
         sym=SY[l]
         Ec=(harmonic(N)+centrifugal(l))
-        hlines(y=Ec,xmin=2.1,xmax=3)
+        hlines(y=Ec,xmin=2.1,xmax=3,color=colors[l])
         text(2.2,Ec,'l={}'.format(l))
         
         
@@ -64,53 +67,18 @@ for N in N_values: #to genrate Harmonic energy term
         if l==0: #to genrate Harmonic energy term + centrifugal energy term + spin orbit coupling term for zero l
             s=1/2
             Eso=E(N,l,s)
-            hlines(y=Eso,xmin=3.1,xmax=5.1,color='olive' )
-            text(3.1,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
+            hlines(y=Eso,xmin=3.1,xmax=5.3,color=colors[l] )
+            text(label_coordinates[l],Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
         
         #to genrate Harmonic energy term + centrifugal energy term + spin orbit coupling term for non-zero ls
-        #Separate if conditions are given for separate l values to make plot with different colors and label the levels at different coordinates in order to be able to distinguish the levels better 
-        if l==1:
+        
+        if l!=0:
             for s in S: 
                 Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1 ,color='green')
-                text(3.3,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
+                hlines(y=Eso,xmin=3.1,xmax=5.3 ,color=colors[l] )
+                text(label_coordinates[l],Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
                 
-        if l==2:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1,color='orange' )
-                text(3.5,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
-        if l==3:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1 ,color='black')
-                text(3.7,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
-        if l==4:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1,color='blue' )
-                text(3.9,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
-        if l==5:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1 ,color='purple')
-                text(4.3,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
-        if l==6:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1,color='grey' )
-                text(4.5,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
-        if l==7:
-            for s in S:
-                Eso=E(N,l,s)
-                hlines(y=Eso,xmin=3.1,xmax=5.1,color='cyan' )
-                text(4.7,Eso, '{}{}{}/2'.format(n(N,l),sym,j(l,s)))
-                
+        
 title('Shell Model')
 ylabel('Energy levels')
 xticks([])
